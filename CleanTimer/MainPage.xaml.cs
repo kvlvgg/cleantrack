278e0c50b5e8c334e7d5a56aa75acc1e7556c481
kovalevgg <kvlv.gg@gmail.com>
2025-03-14 02:57:44 +0300

    init

---
+++ CleanTimer/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanTimer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CleanTimer/Models/HouseholdChore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTimer.Models;
+
+public class HouseholdChore
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DayInterval { get; set; }
+    public DateTime LastDayDone { get; set; }
+    public Guid ParentId { get; set; }
+}
