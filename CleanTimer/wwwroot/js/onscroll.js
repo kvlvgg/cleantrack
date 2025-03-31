1674a07a188a7b1bf0e8230998bb56370d741da4
kvlvgg <kvlv.gg@gmail.com>
2025-03-31 07:08:38 +0300

    appicon, splash, header and edit-section fixes

---
+++ CleanTimer/wwwroot/js/onscroll.js
@@ -0,0 +1,25 @@
+const isActivated = false
+
+function ctOnScroll() {
+    if (isActivated) return;
+
+    document.addEventListener('scroll', e => {
+        const headerEl = document.getElementById('header');
+        const editSection = document.getElementById('edit-section');
+        let lastPageYOffset = 0;
+        const currentPageYOffset = window.pageYOffset;
+
+        if (currentPageYOffset - lastPageYOffset > 0) {
+            if (headerEl) headerEl.style.setProperty('top', 'calc(var(--header-height) * -1)')
+            if (editSection) editSection.style.setProperty('top', '0')
+            lastPageYOffset = currentPageYOffset
+        }
+
+        else {
+            if (headerEl) headerEl.style.setProperty('top', '0')
+            if (editSection) editSection.style.setProperty('top', 'var(--header-height)')
+            lastPageYOffset = currentPageYOffset
+        }
+    });
+}
+
