650d890179caaf8fc0ba05b4f6c9a1783a2d3b20
kvlvgg <kvlv.gg@gmail.com>
2025-04-06 08:55:03 +0300

    Update onscroll.js

--- CleanTrack/wwwroot/js/onscroll.js
+++ CleanTrack/wwwroot/js/onscroll.js
@@ -1,23 +1,24 @@
-const isScrollObserverActivated = false
+const isScrollObserverActivated = false;
 
 function ctOnScroll() {
     if (isScrollObserverActivated) return;
 
     document.addEventListener('scroll', e => {
         const headerEl = document.getElementById('header');
-        const editSection = document.getElementById('chores-panel');
+        const summaryPanel = document.getElementById('summary-panel');
+
         let lastPageYOffset = 0;
         const currentPageYOffset = window.pageYOffset;
 
         if (currentPageYOffset - lastPageYOffset > 0) {
-            if (headerEl) headerEl.style.setProperty('top', 'calc(var(--header-height) * -1)')
-            if (editSection) editSection.style.setProperty('top', '0')
+            if (headerEl) headerEl.style.setProperty('top', 'calc(var(--header-height) * -1)');
+            if (summaryPanel) summaryPanel.style.setProperty('top', '0');
             lastPageYOffset = currentPageYOffset
         }
 
         else {
-            if (headerEl) headerEl.style.setProperty('top', '0')
-            if (editSection) editSection.style.setProperty('top', 'var(--header-height)')
+            if (headerEl) headerEl.style.setProperty('top', '0');
+            if (summaryPanel) summaryPanel.style.setProperty('top', 'var(--header-height)');
             lastPageYOffset = currentPageYOffset
         }
     });
